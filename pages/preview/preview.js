db1a6dc152d031addab84732108b65f93d357265
sen <491469748@qq.com>
2018-06-08 16:26:01 +0800

    Reconstruct preview page

--- pages/preview/preview.js
+++ pages/preview/preview.js
@@ -25,7 +25,54 @@ Page({
     console.log(options.photoPath)
   },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
 
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
   commentInput(input){
     this.setData({
       msg:input.detail.value
@@ -57,18 +104,10 @@ Page({
             'description': that.data.description,
             'msg': that.data.msg
           },
-          success:res=>{
-            wx.showToast({
-              title: '上传成功',
-              icon: 'success',
-              duration: 2000
-            })
-          }
         });
         wx.navigateBack({
-          delta: 1
+          delta:1
         })
-
       },
       fail: function (res) {
         console.log(res.errMsg)
