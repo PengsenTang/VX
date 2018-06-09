a425d41251ac2c62370abe965c657e6058e4fe60
Chenchen Zhuo <crf_moonlight@live.com>
2018-06-09 14:08:27 +0800

    Query page UI.

--- pages/query/query.js
+++ pages/query/query.js
@@ -12,7 +12,6 @@ for(let i=1;i<5;i++){
 var markersData = [];
 Page({
   data: {
-    array:[1,2,3,4,5],
     index:1,
     markers: [],
     circles: [],
@@ -23,13 +22,7 @@ Page({
     scale: 18,
     maxScale: 20,
     minScale: 16,
-    currentPoint:'',
-    currentRate:'',
-    // 20 经度 0.002 
-    // 19 经度 0.004 直径 350m
-    // 18 经度 0.008      700m
-    // 17 经度 0.016      1500m
-    // 16 经度 0.032      3000m
+    currentPoint: null,
     mapContext: null,
     photoPath: '',
     uploadUrl: 'https://www.wantcu.top/highlight/newHighlight',
@@ -41,7 +34,14 @@ Page({
     msg: 'msg',
     showCamera:false,
     showModalStatus:false,
-    showMap:true
+    animationData: null,
+    starStyle: [
+      'star_none',
+      'star_none',
+      'star_none',
+      'star_none',
+      'star_none',
+      ]
   },
   onLoad: function () {
     var that = this;
@@ -54,8 +54,7 @@ Page({
           longitude: res.longitude,
         })
       }
-    })  
-
+    })
   },
   bindPickerChange:function(e){
     var realvalue = e.detail.value
@@ -63,33 +62,6 @@ Page({
     this.setData({
       index:realvalue
      })
-  },
-  submit_rate:function(){
-    var that = this
-    lid = markersData[this.data.currentPoint]['lid']
-    wx.request({
-      url: that.data.rateUrl,
-      method: 'POST',
-      data: {
-        rate: that.data.index+1,
-        locationid:lid
-      },
-      header: {
-        'content-type': 'application/json',
-        'Cookie': Session.get()
-      },
-      success: res => {
-        that.setData({
-          currentRate:that.data.index+1
-        })
-        this.hideModal();
-        wx.showToast({
-          title: '评分成功',
-          icon: 'success',
-          duration:2000
-        })
-      }
-    })
   },
   slider2change: function(e){
     var that = this;
@@ -102,7 +74,7 @@ Page({
     }
   },
   // 点击marker
-  makertap: function (e) {
+  markertap: function (e) {
     var id = e.markerId;
     var that = this;
     that.setData({
@@ -110,9 +82,7 @@ Page({
       showModalStatus:true,
       showMap:false,
       currentPoint:id,
-      currentRate:markersData[id].yourrate
     })
-    console.log(that.data.currentRate)
     that.showMarkerInfo(markersData, id);
     that.changeMarkerColor(markersData, id);
   },
@@ -138,6 +108,65 @@ Page({
         snapshot: data[i].snapshot
       }
     });
+    that.setStarStyle(data[i].yourrate);
+  },
+  // 用户评分
+  // 设置星星的样式
+  setStarStyle: function (stars) {
+    var that = this;
+    var styles = [];
+    for (var i = 1; i <= 5; i++) {
+      if (i <= stars) styles[i - 1] = 'star_full';
+      else styles[i - 1] = 'star_none';
+    }
+    that.setData({
+      starStyle : styles
+    });
+  },
+  setStarOne: function () {
+    var that = this;
+    that.submitRate(1);
+  },
+  setStarTwo: function () {
+    var that = this;
+    that.submitRate(2);
+  },
+  setStarThree: function () {
+    var that = this;
+    that.submitRate(3);
+  },
+  setStarFour: function () {
+    var that = this;
+    that.submitRate(4);
+  },
+  setStarFive: function () {
+    var that = this;
+    that.submitRate(5);
+  },
+  // TODO:
+  submitRate: function (stars) {
+    var that = this
+    lid = markersData[this.data.currentPoint]['lid']
+    wx.request({
+      url: that.data.rateUrl,
+      method: 'POST',
+      data: {
+        rate: stars,
+        locationid: lid
+      },
+      header: {
+        'content-type': 'application/json',
+        'Cookie': Session.get()
+      },
+      success: res => {
+        that.setStarStyle(stars);
+        wx.showToast({
+          title: '评分成功',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    })
   },
   // 修改点击marker后的图片
   changeMarkerColor: function (data, i) {
@@ -204,7 +233,7 @@ Page({
   newPoint(){
     var that = this
     wx.showActionSheet({
-      itemList: ['chooseImage','take a photo'],
+      itemList: ['打开相册','拍摄照片'],
       success: function(res){
         if(res.tapIndex == 0){
           that.chooseImage();
@@ -252,40 +281,21 @@ Page({
     })
   },
   showModal: function () {
-    // 显示遮罩层
-    var animation = wx.createAnimation({
-      duration: 200,
-      timingFunction: "linear",
-      delay: 0
-    })
-    this.animation = animation
-    animation.translateY(300).step()
     this.setData({
-      animationData: animation.export(),
       showModalStatus: true
     })
-    setTimeout(function () {
-      animation.translateY(0).step()
-      this.setData({
-        animationData: animation.export()
-      })
-    }.bind(this), 200)
   },
   hideModal: function () {
-    // 隐藏遮罩层
     var animation = wx.createAnimation({
-      duration: 200,
+      duration: 400,
       timingFunction: "linear",
       delay: 0
     })
-    this.animation = animation
-    animation.translateY(300).step()
+    animation.translateX(800).step()
     this.setData({
       animationData: animation.export(),
-      showMap:true
     })
     setTimeout(function () {
-      animation.translateY(0).step()
       this.setData({
         animationData: animation.export(),
         showModalStatus: false
